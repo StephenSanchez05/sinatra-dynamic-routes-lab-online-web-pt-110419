89db79e85373a3554be68af1a49309e2cef9103f
Stephen Sanchez <stephensanchez05@gmail.com>
2020-02-28 18:51:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @phrase = params[:phrase]
     @number = params[:number].to_i
-    <%= print "#{@phrase}" * @number
+    <%= print "#{@phrase}" * @number %>
   end
   
   
