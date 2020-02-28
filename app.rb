b784fdb07088103e13284f59e3fc906a58a8e432
Stephen Sanchez <stephensanchez05@gmail.com>
2020-02-28 19:00:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
   
   get '/:operation/:number1/:number2' do
     @solution = params[:number1].to_i params[:operation] params[:number2]to_i
-    "#{@solution}""
+    "#{@solution}"
     end
   
   
